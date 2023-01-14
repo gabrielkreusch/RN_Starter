e4e149a59406e98f36aaefb0ffd6492aa7159a52
Gabriel Kreusch <gbr.kreusch@gmail.com>
2023-01-14 19:23:14 -0300

    Initial commit

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const ComponentsScreen = () => {
+    const myName = 'Gabriel';
+
+    return <View>
+        <Text style={styles.title}>Getting started with React Native!</Text>
+        <Text>My name is {myName}</Text>
+    </View>;
+};
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 45
+    },
+    subtitle: {
+        fontSize: 20
+    }
+});
+
+export default ComponentsScreen;
